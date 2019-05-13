89b72c71a38f8ce03ba07b47c03e3643f522461a
Isak Jägberg <ijagberg@gmail.com>
2019-05-13 23:12:54 +0200

    Add structs

--- src/exact_cover.rs
+++ src/exact_cover.rs
@@ -5,8 +5,16 @@ struct DataObject {
     down: Rc<DataObject>,
     header: Rc<ColumnObject>,
 }
+
 struct ColumnObject {
     data: Rc<DataObject>,
     size: usize,
     name: String,
+}
+
+struct ExactCover {
+    root: ColumnObject
+}
+
+impl ExactCover {
 }
