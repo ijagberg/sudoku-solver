9b8323b0cad10031e346412e13a19450747428f3
Isak Jägberg <ijagberg@gmail.com>
2020-12-13 23:41:12 +0100

    Update dependencies, add a couple tests

--- tests/solver.rs
+++ tests/solver.rs
@@ -84,6 +84,22 @@ mod tests {
         instance
     }
 
+    fn get_solvable_9x9_test_instance_2() -> Sudoku {
+        let mut instance = Sudoku::new(9, 3, 3).unwrap();
+        instance.populate_from_str(
+            r#"_ _ 3 _ 2 _ 6 _ _
+               9 _ _ 3 _ 5 _ _ 1
+               _ _ 1 8 _ 6 4 _ _
+               _ _ 8 1 _ 2 9 _ _
+               7 _ _ _ _ _ _ _ 8
+               _ _ 6 7 _ 8 2 _ _
+               _ _ 2 6 _ 9 5 _ _
+               8 _ _ 2 _ 3 _ _ 9
+               _ _ 5 _ 1 _ 3 _ _"#,
+        );
+        instance
+    }
+
     fn get_unsolvable_9x9_test_instance() -> Sudoku {
         let mut instance = Sudoku::new(9, 3, 3).unwrap();
         instance.populate_from_str(
@@ -132,9 +148,13 @@ mod tests {
     #[test]
     fn test_solvable_9x9() {
         let unsolved = get_solvable_9x9_test_instance();
+        println!("unsolved: ");
+        println!("{:?}", unsolved);
         assert_eq!(false, unsolved.is_solved());
         let solved = Backtrack::solve(unsolved);
         assert_eq!(true, solved.is_ok());
+        println!("solved: ");
+        println!("{:?}", solved.unwrap());
     }
 
     #[test]
